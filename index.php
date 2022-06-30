3d5539276677168632bf6a5ff3eab148355e31f7
stranger <nick.ivushkin@gmail.com>
2022-06-30 20:43:19 +0300

    A little bit of changes

--- index.php
+++ index.php
@@ -1,15 +1,16 @@
 <?php
-$title = 'Мой сайт';
-$name = 'Неопознанный енот';
+$title = 'Тестовый сайт';
+$name = 'неопознанный Ивушкин Никита';
 $image = 'img/unknown-raccoon.svg';
-$email = 'enot_neopoznanniy@gmail.com';
-$phone = '+79876543210';
+$email = 'ivushkinnv@gmail.com';
+$phone = '+79876783214';
 
 require('components/header.php');
+require('components/news.php');
+require('components/photos.php');
 require('components/about.php');
 require('components/status.php');
-require('components/photos.php');
-require('components/news.php');
 require('components/quotes.php');
 require('components/contacts.php');
 require('components/footer.php');
+?>
